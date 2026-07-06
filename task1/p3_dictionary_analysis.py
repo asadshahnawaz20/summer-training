22aa456148b4d0ec90e81d1f9da6f9f1958f85ba
asadshahnawaz <drasadchanna657@gmail.com>
2026-07-06 14:42:24 +0500

    Apply Ruff formatting

--- task1/p3_dictionary_analysis.py
+++ task1/p3_dictionary_analysis.py
@@ -23,11 +23,7 @@ patients = {
 
 def get_patient_city(patient_id):
     """Return the city for a given patient ID."""
-    return (
-        patients.get(patient_id, {})
-        .get("contact", {})
-        .get("city")
-    )
+    return patients.get(patient_id, {}).get("contact", {}).get("city")
 
 
 def update_patient_condition(patient_id, new_condition):
